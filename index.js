7379e102c87ef406d65603b4fe72744b071c2c08
mayank011 <mv@rapidinnovation.dev>
2023-06-26 17:53:31 +0530

    -

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ function sendEveryNSeconds() {
 // "(0-59)  (0-59) (0-23) (1-31)   (1-12)  (0-7)  "
 
 // Logs info every 5th second
-const job = schedule.scheduleJob("*/5 * * * * *", sendEveryNSeconds);
+// const job = schedule.scheduleJob("*/5 * * * * *", sendEveryNSeconds);
 
 // Send Room service reminder Monday to Friday at 9:00AM
 const roomServiceRule = new schedule.RecurrenceRule();
 roomServiceRule.dayOfWeek = [1, 2, 3, 4, 5];
-roomServiceRule.hour = 9; // 0 - 23
-roomServiceRule.minute = 0; // 0 - 59
+roomServiceRule.hour = 16; // 0 - 23
+roomServiceRule.minute = 40; // 0 - 59
 const roomService = schedule.scheduleJob(roomServiceRule, notifyRoomService);
 
 // Collect system statistics every day at 12:00AM
